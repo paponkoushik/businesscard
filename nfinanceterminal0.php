61f9f1eda38d401b71eee7d2b853ae6ee933a93a
Koushik <paponkoushik@gmail.com>
2025-04-18 12:14:56 +0600

    new webshop

---
+++ nfinanceterminal0.php
@@ -0,0 +1,9 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class Nfinanceterminal0 extends BaseTheme
+{
+    // Access plugin events in this class
+}
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  darkMode: 'class',
+  content: [
+    '../../pages/**/*.md',
+    '../../../../themes/base-theme/js/**/*.js',
+    '../../../../themes/base-theme/templates/**/*.twig',
+    './blueprints/**/*.yaml',
+    './templates/**/*.twig',
+    './nfinanceterminal0.yaml',
+    './nfinanceterminal0.php'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['LibreFranklin', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'brand': 'rgb(var(--color-brand) / <alpha-value>)',
+        'brand-light': 'rgb(var(--color-brand-light) / <alpha-value>)',
+        'brand-dark': 'rgb(var(--color-brand-dark) / <alpha-value>)',
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-invert-body': theme('colors.gray.100'),
+          }
+        }
+      })
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-debug-screens'),
+  ]
+}
